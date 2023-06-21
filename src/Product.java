6b589db8ee48b76149ea297fc49abd74a4533510
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-06-21 20:11:37 +0600

    initialized

--- src/Product.java
+++ src/Product.java
@@ -4,7 +4,8 @@ public class Product {
     private String place;
     private int warranty;
 
-    public Product() {
+    public Product(){
+        
     }
 
     public Product(String name, String type, String place, int warranty) {
